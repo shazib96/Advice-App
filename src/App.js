53c85fe2f5b05445289dc9234ffd92023eadec52
Shazib Mustafa <shazijoiya@gmail.com>
2021-09-21 19:36:26 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -25,10 +25,12 @@ class App extends React.Component {
   }
 
   render() {
+    const { advice } =this.state;
+    
     return (
       <div className="app">
         <div className="card">
-          <h1 className="heading">{this.state.advice}</h1>
+          <h1 className="heading">{advice}</h1>
           <button className="button" onClick={this.fetchAdvice}>
             <span>GIVE ME ADVICE!</span>
           </button>
